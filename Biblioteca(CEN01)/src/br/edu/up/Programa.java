087813691b27e5f4b584b8043ae66f6c3dcadcd1
NyczDev <nicolasyasumoto@gmail.com>
2024-04-18 10:12:15 -0300

    Update Livro 2 Bug

--- Biblioteca(CEN01)/src/br/edu/up/Programa.java
+++ Biblioteca(CEN01)/src/br/edu/up/Programa.java
@@ -20,6 +20,17 @@ public class Programa {
             System.out.println("ISNB: " + livro01.getIsbn());
             System.out.println("Ano: " + livro01.getAno());
             System.out.println();
+
+            System.out.println("Livro 02:");
+            System.out.println("Código: " + livro02.getCodigo());
+            System.out.println("Título: " + livro02.getTitulo());
+            System.out.println("Autores: ");
+            for (String autor : livro02.getAutores()) {
+            System.out.println("- " + autor);
+            }
+            System.out.println("ISBN: " + livro02.getIsbn());
+            System.out.println("Ano: " + livro02.getAno());
     
     }
+    
 }
