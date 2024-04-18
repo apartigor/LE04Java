969f306e36bacbd3458c805ac8186a53ced167eb
NyczDev <nicolasyasumoto@gmail.com>
2024-04-18 11:02:27 -0300

    Update CEN02 teste

--- DistanciaPonto(CEN02)/src/br/edu/up/Programa.java
+++ DistanciaPonto(CEN02)/src/br/edu/up/Programa.java
@@ -1,8 +1,16 @@
 package br.edu.up;
 import java.lang.Math;
 
+import br.edu.up.modelos.Livro;
+
 public class Programa {
     public static void main(String[] args) {
 
+
+
+
+        Livro livro01 = new Livro("1598FHK", "Core Java 2",
+                new String[] { "Cay S. Horstmann", "Gary Cornell" },
+                "0130819336", 2005);
     }
 }
