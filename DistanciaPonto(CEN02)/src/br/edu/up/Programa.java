bef0f105ac8c0ec924ea08fa7d4a089492917f52
apartigor <contact.jojigor@gmail.com>
2024-04-18 11:07:36 -0300

    Update CEN02

--- DistanciaPonto(CEN02)/src/br/edu/up/Programa.java
+++ DistanciaPonto(CEN02)/src/br/edu/up/Programa.java
@@ -6,5 +6,10 @@ import br.edu.up.modelos.Ponto;
 public class Programa {
     public static void main(String[] args) {
         Ponto p1 = new Ponto(0, 0);
+
+    }
+
+    public static void salveGuys() {
+        System.out.println("nada nao");
     }
 }
