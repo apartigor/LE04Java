6cf41610c1fdf37097505e7d34a5c786a66302bb
NyczDev <nicolasyasumoto@gmail.com>
2024-04-18 11:18:36 -0300

    Update CEN02

--- DistanciaPonto(CEN02)/src/br/edu/up/Programa.java
+++ DistanciaPonto(CEN02)/src/br/edu/up/Programa.java
@@ -1,20 +1,13 @@
 package br.edu.up;
 
-import java.lang.Math;
 import br.edu.up.modelos.Ponto;
 
 public class Programa {
     public static void main(String[] args) {
-        Ponto p1 = new Ponto(0, 0);
+        Ponto ponto1 = new Ponto();
+        Ponto ponto2 = new Ponto(2, 5);
 
-    }
-
-    public static void salveGuys() {
-        System.out.println("nada nao");
-
-        
-    }
-    public static void salve() {
-        System.out.println("teste 12345678");
     }
 }
+
+
