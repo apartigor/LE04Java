7f91c9ee9b92210b52f3a12216d288664bd8d64b
NyczDev <nicolasyasumoto@gmail.com>
2024-04-24 10:12:04 -0300

    Update

--- Agenda(CEN03)/src/br/edu/up/modelos/Mes.java
+++ Agenda(CEN03)/src/br/edu/up/modelos/Mes.java
@@ -1,5 +1,31 @@
 package br.edu.up.modelos;
 
 public class Mes {
-    
+    private int nome;
+    private int qtdeDias;
+    private Dia[]dias;
+
+    public Mes(int nome, int qtdeDias) {
+        this.nome = nome;
+        this.qtdeDias = qtdeDias;
+    }
+
+    public void adicionarCompromisso(Compromisso comp, Dia[]dias) {
+
+    }
+    public void adicionarCompromisso(String pessoa, String local, String assunto, int hora, int diaMes) {
+
+    }
+
+    public void excluirCompromisso(int diaMes, int hora) {
+
+    }
+
+    public String listarCompromisso(int diaMes) {
+        return;
+    }
+
+    public String listarCompromisso() {
+        return;
+    }
 }
