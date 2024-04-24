551af18b618415803e3fbf2a9693d08e27b7797b
NyczDev <nicolasyasumoto@gmail.com>
2024-04-24 10:02:36 -0300

    Update

--- Agenda(CEN03)/src/br/edu/up/modelos/Dia.java
+++ Agenda(CEN03)/src/br/edu/up/modelos/Dia.java
@@ -1,5 +1,31 @@
 package br.edu.up.modelos;
 
 public class Dia {
-    
+    private int diaMes;
+    private Compromisso[] compromissos;
+
+
+    public Dia(int diaMes) {
+        this.diaMes = diaMes;
+
+    }
+
+    public void adicionarCompromisso(Compromisso comp) {
+
+    }
+
+    public Compromisso consultarCompromisso(int hora) {
+        
+        return;
+    }
+
+    public void excluirCompromisso(int hora) {
+        
+    }
+
+    public String listarCompromisso() {
+
+        return;
+    }
+
 }
