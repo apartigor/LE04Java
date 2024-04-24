9ee0c60fcf9d2120e1952417b39fb04a68e40032
apartigor <contact.jojigor@gmail.com>
2024-04-24 10:02:16 -0300

    Update CEN03

--- Agenda(CEN03)/src/br/edu/up/modelos/Ano.java
+++ Agenda(CEN03)/src/br/edu/up/modelos/Ano.java
@@ -1,5 +1,29 @@
 package br.edu.up.modelos;
 
 public class Ano {
-    
+    private int ano;
+    private boolean bissexto;
+    private Mes[] meses;
+
+    public Ano(int ano, boolean bissexto) {
+        this.ano = ano;
+        this.bissexto = bissexto;
+    }
+
+    public void adicionarMes(Mes mes) {
+
+    }
+
+    public void excluirCompromisso(String nomeMes, int diaMes, int hora) {
+
+    }
+
+    public String listarCompromissos(String nomeMes) {
+        return;
+    }
+
+    public String listarCompromissos() {
+        return;
+    }
+
 }
