9e41a5db86e319800d523ec379aeda62c3bbda26
apartigor <contact.jojigor@gmail.com>
2024-04-24 09:22:00 -0300

    Update CEN02

--- DistanciaPonto(CEN02)/src/br/edu/up/Programa.java
+++ DistanciaPonto(CEN02)/src/br/edu/up/Programa.java
@@ -7,6 +7,12 @@ public class Programa {
         Ponto ponto1 = new Ponto();
         Ponto ponto2 = new Ponto(2, 5);
 
+        double distp1p2 = ponto1.calcularDistancia(ponto2);
+
+        System.out.printf("Distância do Ponto 1 ao Ponto 2: %.3f", distp1p2);
+
+        
+
     }
 }
 
